670e6a416886a0b03143b7d25259400bce850eb9
kmagaoay <kelly.magaoay@zoom.us>
2021-10-26 13:20:46 -0700

    deploying to github

--- src/App.js
+++ src/App.js
@@ -2,31 +2,20 @@ import React from 'react';
 
 import './App.css';
 
-declare var ZoomMtg
-
-ZoomMtg.setZoomJSLib('https://source.zoom.us/2.0.1/lib', '/av');
-
-ZoomMtg.preLoadWasm();
-ZoomMtg.prepareWebSDK();
-// loads language files, also passes any error messages to the ui
-ZoomMtg.i18n.load('en-US');
-ZoomMtg.i18n.reload('en-US');
+declare var ZoomMtgEmbedded
 
 function App() {
 
+  const client = ZoomMtgEmbedded.createClient();
+
   // setup your signature endpoint here: https://github.com/zoom/meetingsdk-sample-signature-node.js
-  var signatureEndpoint = ''
-  var apiKey = ''
-  var meetingNumber = '123456789'
+  var signatureEndpoint = 'https://siganturezoom.herokuapp.com/'
+  var apiKey = 'qzloYzd5SRKT9ve2PMc88Q'
+  var meetingNumber = new URLSearchParams(window.location.search).get('meetingnumber')
   var role = 0
-  var leaveUrl = 'http://localhost:3000'
-  var userName = 'React'
+  var userName = 'MTGWEB201SDK'
   var userEmail = ''
-  var passWord = ''
-  // pass in the registrant's token if your meeting or webinar requires registration. More info here:
-  // Meetings: https://marketplace.zoom.us/docs/sdk/native-sdks/web/build/meetings/join#join-registered
-  // Webinars: https://marketplace.zoom.us/docs/sdk/native-sdks/web/build/webinars/join#join-registered-webinar
-  var registrantToken = ''
+  var passWord = new URLSearchParams(window.location.search).get('passcode')
 
   function getSignature(e) {
     e.preventDefault();
@@ -47,33 +36,36 @@ function App() {
   }
 
   function startMeeting(signature) {
-    document.getElementById('zmmtg-root').style.display = 'block'
 
-    ZoomMtg.init({
-      leaveUrl: leaveUrl,
-      success: (success) => {
-        console.log(success)
+    let meetingSDKElement = document.getElementById('meetingSDKElement');
 
-        ZoomMtg.join({
-          signature: signature,
-          meetingNumber: meetingNumber,
-          userName: userName,
-          apiKey: apiKey,
-          userEmail: userEmail,
-          passWord: passWord,
-          tk: registrantToken,
-          success: (success) => {
-            console.log(success)
-          },
-          error: (error) => {
-            console.log(error)
-          }
-        })
-
-      },
-      error: (error) => {
-        console.log(error)
+    client.init({
+      debug: true,
+      zoomAppRoot: meetingSDKElement,
+      language: 'en-US',
+      customize: {
+        meetingInfo: ['topic', 'host', 'mn', 'pwd', 'telPwd', 'invite', 'participant', 'dc', 'enctype'],
+        toolbar: {
+          buttons: [
+            {
+              text: 'Custom Button',
+              className: 'CustomButton',
+              onClick: () => {
+                console.log('custom button');
+              }
+            }
+          ]
+        }
       }
+    });
+
+    client.join({
+    	apiKey: apiKey,
+    	signature: signature,
+    	meetingNumber: meetingNumber,
+    	password: passWord,
+    	userName: userName,
+      userEmail: userEmail
     })
   }
 
@@ -81,6 +73,11 @@ function App() {
     <div className="App">
       <main>
         <h1>Zoom Meeting SDK Sample React</h1>
+
+        {/* For Component View */}
+        <div id="meetingSDKElement">
+          {/* Zoom Meeting SDK Component View Rendered Here */}
+        </div>
 
         <button onClick={getSignature}>Join Meeting</button>
       </main>
