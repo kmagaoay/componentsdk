416548a435d33e798bf71aa66a01e63f99592ca7
kmagaoay <kelly.magaoay@zoom.us>
2021-12-16 15:57:19 -0800

    update 211

--- src/App.js
+++ src/App.js
@@ -21,8 +21,8 @@ function App() {
   var signatureEndpoint = 'https://siganturezoom.herokuapp.com/'
   var apiKey = 'qzloYzd5SRKT9ve2PMc88Q'
   var meetingNumber = new URLSearchParams(window.location.search).get('meetingnumber')
-  var role = 0
-  var userName = 'MTGWEB201SDK'
+  var role = 1
+  var userName = 'Trainee'
   var userEmail = ''
   var passWord = new URLSearchParams(window.location.search).get('passcode')
 
