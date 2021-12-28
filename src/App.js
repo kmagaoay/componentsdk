9ea2ac9cd1f6202d94d68b2c885009b67460092b
kmagaoay <kelly.magaoay@zoom.us>
2021-12-28 09:34:02 -0800

    role name change

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   var apiKey = 'qzloYzd5SRKT9ve2PMc88Q'
   var meetingNumber = new URLSearchParams(window.location.search).get('meetingnumber')
   var role = 0
-  var userName = 'Trainee'
+  var userName = 'Participant'
   var userEmail = ''
   var passWord = new URLSearchParams(window.location.search).get('passcode')
 
